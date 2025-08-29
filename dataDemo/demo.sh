fc1e982bc85af168a4bda001eac11a7cb78f15a0
YCMavis <zycpppppppp@163.com>
2025-08-30 00:42:33 +0800

    version1.0.0

--- dataDemo/demo.sh
+++ dataDemo/demo.sh
@@ -2,4 +2,4 @@ lreads=sim-ecoli/demo_long.fa
 sreads=sim-ecoli/demo_short.fa
 
 tar -zxvf sim-ecoli.tar.gz
-../DADEC -s $sreads -l $lreads -o ecoli.fa -S 1 -t 32
+../DADEC -s $sreads -l $lreads -o corrected.fa -S 1 -t 32
